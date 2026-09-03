9774ac165be2b30c17a8b32647a8bb217fab3f1f
AndyGar01 <agarcial32@miumg.edu.gt>
2026-09-03 00:49:26 -0600

    Capa Vista

---
+++ Taller_MVC_1/CapaVista_MVC1/frmPrincipal.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CapaVista_MVC1
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvConsultaTabla = new System.Windows.Forms.DataGridView();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaTabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvConsultaTabla
+            // 
+            this.dgvConsultaTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsultaTabla.Location = new System.Drawing.Point(12, 157);
+            this.dgvConsultaTabla.Name = "dgvConsultaTabla";
+            this.dgvConsultaTabla.RowHeadersWidth = 51;
+            this.dgvConsultaTabla.RowTemplate.Height = 24;
+            this.dgvConsultaTabla.Size = new System.Drawing.Size(776, 281);
+            this.dgvConsultaTabla.TabIndex = 0;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(301, 42);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(178, 78);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "Consultar Datos";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.dgvConsultaTabla);
+            this.Name = "frmPrincipal";
+            this.Text = "frmPrincipal";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaTabla)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvConsultaTabla;
+        private System.Windows.Forms.Button btnConsultar;
+    }
+}
---
+++ Taller_MVC_1/CapaVista_MVC1/frmPrincipal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador_MVC1;
+
+namespace CapaVista_MVC1
+{
+    public partial class frmPrincipal : Form
+    {
+        string nombreTabla = "cliente";
+        Controlador controlador = new Controlador();    
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+            
+        public void actualizarDataGridView()
+        {
+            DataTable dtVista = controlador.llenarDgv(nombreTabla);
+            dgvConsultaTabla.DataSource = dtVista;
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            actualizarDataGridView();
+        }
+    }
+}
